ee29990c4b28efe0a66b2ef61622c36a3f586331
ar2244450-a11y <ar2244450@gmail.com>
2026-08-07 17:43:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ document.getElementById('mainForm').addEventListener('submit', async (e) => {
     }));
  
     const payload = {
-      api_key: 'mlys_7hK2pQ9xR4vN8wZ3tY6',
+     
       merchant_name: document.getElementById('merchantName').value,
       merchant_phone: document.getElementById('merchantPhone').value,
       products
@@ -126,9 +126,11 @@ document.getElementById('mainForm').addEventListener('submit', async (e) => {
  
     const res = await fetch(WEBHOOK_URL, {
   method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
+ headers: {
+  'Content-Type': 'application/json',
+  'X-API-Key': 'mlys_7hK2pQ9xR4vN8wZ3tY6'
+},
+
   body: JSON.stringify(payload)
 });
  
